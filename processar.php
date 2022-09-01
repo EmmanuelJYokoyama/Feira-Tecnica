d7d1f5954b5caacc3e0332197ab02a4db6ac70dd
Pedro Henrique Mota <83295376+Costelinh4@users.noreply.github.com>
2022-09-01 00:28:46 -0300

    atualizado//

--- processar.php
+++ processar.php
@@ -1,50 +1,39 @@
 <?php
 
 include "conexao.php";
-	$banco = new Notas();
+    $banco = new Notas();
 
-	if(isset($_POST['nota1'])){
-		//Variaveis para a Avaliação
-		$Apresentacao = $_POST['nota1'];
-		$Relevancia = $_POST['nota2'];
-		$Utilidade = $_POST['nota3'];
-		$comentario = $_POST["coment"];
-		$res = $banco->inserirNOTA($Apresentacao, $Relevancia, $Utilidade, $comentario);
-	}
-	
-	if(isset($_POST['nome'])){
-		//Váriaveis para o cadastro do Formulario
-		$nome = $_POST['nome'];
-		$arraynome = explode(",", $_POST['nome']);
-		$matriculaCAP = $_POST['matricula'];
-		$turma = $_POST['turma'];
-		$emailCAP = $_POST['email'];
-		$titulo = $_POST['titulo'];
-		$resumo = $_POST['resumo'];
-		$area = $_POST['area'];
-		$res = $banco->inserirALUNO($nome, $matriculaCAP, $emailCAP, $turma);
-		$res = $banco->inserirTRABALHO($titulo, $resumo, $area);
-		//$res = $banco->inserirGRUPO();
-		//$res = $banco->inserirTURMA($turma);
-		//$res = $banco->inserirCURSO($area);
-		for ($i=0; $i<count($arraynome); $i++)
-		{
-			$nome = $arraynome[$i];
-			echo count($arraynome);
-		}
-		
-		
-		
-	}
-	
+    if(isset($_POST['nota1'])){
 
-	
-	
-	
-	
-	
+        //Variaveis para a Avaliação
+        $Apresentacao = $_POST['nota1'];
+        $Relevancia = $_POST['nota2'];
+        $Utilidade = $_POST['nota3'];
+        $comentario = $_POST["coment"];
+        $area = $_POST['area'];
+        $res = $banco->inserirNOTA($Apresentacao, $Relevancia, $Utilidade, $comentario, $area);
+    }
+    
+    if(isset($_POST['nome'])){
 
+        //Váriaveis para o cadastro do Formulario
 
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $matricula = $_POST['matricula'];
+        $turma = $_POST['turma'];
+        $titulo = $_POST['titulo'];
+        $resumo = $_POST['resumo'];
+        $area = $_POST['area'];
 
-	
-	
+        $res = $banco->inserirALUNO($nome, $matricula, $email, $turma);
+        $res = $banco->inserirTRABALHO($titulo, $resumo, $area);
+        $res = $banco->inserirGRUPO($matricula, $titulo);
+
+        //$res = $banco->inserirGRUPO();
+        //$res = $banco->inserirTURMA($turma);
+        //$res = $banco->inserirCURSO($area);
+       
+          
+        
+    }
